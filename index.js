9288085edd9287b47d42ae5960255b734269a5cd
rsposito13 <164542215+rsposito13@users.noreply.github.com>
2025-02-14 23:56:41 +0000

    terminado

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-import express from "express";      // Requisição do pacote do express
-const app = express();              // Instancia o Express
-const port = 3000;                  // Define a porta
 
-app.get("/", (req, res) => {        // Cria a rota da raiz do projeto
-  console.log("Rota GET/ solicitada");
+//index.js
+import dotenv from "dotenv";
+import express from "express";
+import { selectUsuarios } from "./bd.js";
+
+dotenv.config();
+
+const app = express(); // Instancia o Express
+const port = 3000; // Define a porta
+
+app.get("/", (req, res) => {
+  console.log("Rota / solicitada");
+  // Cria a rota da raiz do projeto
   res.json({
-    nome: "Rhuan Spósito de Almeida",      // Substitua pelo seu nome
+    nome: "Rhuan Spósito de Almeida", // Substitua pelo seu nome
   });
 });
 
-app.listen(port, () => {            // Um socket para "escutar" as requisições
+app.get("/usuario", async (req, res) => {
+  try {
+    const usuarios = await selectUsuarios();
+    res.json(usuarios);
+  } catch (error) {
+    res.status(error.status || 500).json({ message: error.message || "Erro!" });
+  }
+
+  console.log("Rota GET/usuario solicitada");
+});
+
+app.listen(port, () => {
+  // Um socket para "escutar" as requisições
   console.log(`Serviço escutando na porta:  ${port}`);
 });
